85ff4331ce83c247cd90f799cdbfcb2bfee16c5e
Rahul Pandey <140502440+pdyrahul@users.noreply.github.com>
2025-11-09 13:37:11 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "/api/api/books"; // 👈 proxy will forward this
+const API_BASE = "/api/books"; // 👈 proxy will forward this
 
 export const fetchBooks = () => axios.get(API_BASE);
 export const addBook = (book) => axios.post(API_BASE, book);
