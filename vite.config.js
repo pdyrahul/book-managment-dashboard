02dd1f8990aa979570a4e37ab620074130ad2374
Rahul Pandey <140502440+pdyrahul@users.noreply.github.com>
2025-11-09 13:22:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://books-api.free.beeceptor.com', // backend target
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // keep same prefix
+      },
+    },
+  },
 })
